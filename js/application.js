07fffcc5bf7b7726c77b62d97fec884ef7a4e7c7
Matej <matejpale@gmail.com>
2021-06-30 15:20:12 +0200

    Integrated API into Javascript

--- js/application.js
+++ js/application.js
@@ -1,6 +1,7 @@
-//Add new task function - may need to adjust task numbering
-$(document).ready(function () {
-  //LIVE DATE
+//ATD API ID: id:108
+//URL: https://altcademy-to-do-list-api.herokuapp.com/
+
+let liveDate = function () {
   let currentTime = new Date();
   let options = { month: "long" };
   let month = new Intl.DateTimeFormat("en-GB", options).format(currentTime);
@@ -19,30 +20,119 @@ $(document).ready(function () {
   document.getElementById(
     "datetime"
   ).innerHTML = `${weekday}, ${day} ${month} ${year}`;
+};
 
-  //Keeps count of number of items on the list to assign unique IDs
-  var itemAccumulator = $(".list-group-item").length;
-
-  var updateListItemCount = function () {
-    $(".number-of-tasks").html($(".list-group-item").length);
-  };
-  updateListItemCount();
-  var addNewTask = function () {
-    $(".list-group").append(`<li class="list-group-item">
+// GET API REQUEST
+var getAndDisplayAllTasks = function () {
+  $.ajax({
+    type: "GET",
+    url: "https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=108",
+    dataType: "json",
+    success: function (response, textStatus) {
+      $(".list-group").empty();
+      // response is a parsed JavaScript object instead of raw JSON
+      response.tasks.forEach(function (task) {
+        $(".list-group").append(`<li class="list-group-item">
     <input
       class="form-check-input rounded-circle me-1"
-      id="${itemAccumulator + 1}"
+      id="${task.id}"
       type="checkbox"
       value=""
+      data-id="${task.id}"
+      ${task.completed ? "checked" : ""}
     />
-    <label class="form-check-label pt-2" for="${itemAccumulator + 1}">${$(
-      "#newTask"
-    ).val()}</label>
-    <button class="btn btn-sm remove-item border">Remove</button>
+    <label class="form-check-label pt-2" for="${
+      task.id
+    }">${task.content}</label>
+    <button class="btn btn-sm remove-item border" data-id="${
+      task.id
+    }">Remove</button>
   </li>`);
-    return (itemAccumulator += 1);
-  };
+      });
+      updateListItemCount();
+    },
+    error: function (request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    },
+  });
+};
 
+// ADD NEW TASK API REQUEST
+
+var addNewTask = function () {
+  $.ajax({
+    type: "POST",
+    url: "https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=108",
+    contentType: "application/json",
+    dataType: "json",
+    data: JSON.stringify({
+      task: {
+        content: $("#newTask").val(),
+      },
+    }),
+    success: function (response, textStatus) {
+      $("#newTask").val("");
+      getAndDisplayAllTasks();
+    },
+    error: function (request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    },
+  });
+};
+// DELETE API REQUEST
+var removeItem = function (id) {
+  $.ajax({
+    type: "DELETE",
+    url: `https://altcademy-to-do-list-api.herokuapp.com/tasks/${id}?api_key=108`,
+    success: function (response, textStatus) {
+      getAndDisplayAllTasks();
+    },
+    error: function (request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    },
+  });
+};
+
+//MARK COMPLETE (PUT) API REQUEST
+
+var markComplete = function (id) {
+  $.ajax({
+    type: "PUT",
+    url: `https://altcademy-to-do-list-api.herokuapp.com/tasks/${id}/mark_complete?api_key=108`,
+    dataType: "json",
+    success: function (response, textStatus) {
+      getAndDisplayAllTasks();
+    },
+    error: function (request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    },
+  });
+};
+
+//MARK ACTIVE (PUT) API REQUEST
+var markActive = function (id) {
+  $.ajax({
+    type: "PUT",
+    url: `https://altcademy-to-do-list-api.herokuapp.com/tasks/${id}/mark_active?api_key=108`,
+    dataType: "json",
+    success: function (response, textStatus) {
+      console.log("The API request fired successfully");
+      getAndDisplayAllTasks();
+    },
+    error: function (request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    },
+  });
+};
+
+//Keeping count of tasks
+var updateListItemCount = function () {
+  $(".number-of-tasks").html($(".list-group-item").length);
+};
+
+//Add new task function - may need to adjust task numbering
+$(document).ready(function () {
+  //EVENT LISTENERS
   //Add new item by enter keypress
   $("#newTask").keypress(function (event) {
     if (event.keyCode === 13 && $("#newTask").val().length !== 0) {
@@ -50,6 +140,7 @@ $(document).ready(function () {
       addNewTask();
       updateListItemCount();
     } else if (event.keyCode === 13 && $("#newTask").val().length === 0) {
+      event.preventDefault();
       alert("Input can not be left blank");
     }
   });
@@ -59,15 +150,14 @@ $(document).ready(function () {
       addNewTask();
       updateListItemCount();
     } else if ($("#newTask").val().length === 0) {
+      event.preventDefault();
       alert("Input can not be left blank");
     }
   });
-
   //Remove button working with new items too
   $(document).on("click", ".remove-item", function () {
-    $(this).parentsUntil(".list-group").remove();
+    removeItem($(this).data("id"));
     updateListItemCount();
-    console.log("Remove button was hit.");
   });
 
   //Show active-only tasks
@@ -104,4 +194,17 @@ $(document).ready(function () {
     $(this).addClass("selected");
     $(this).siblings().removeClass("selected");
   });
+
+  //Change event that checks the checkbox is checked / unchecked
+  $(document).on("change", ".form-check-input", function () {
+    if (this.checked) {
+      markComplete($(this).data("id"));
+    } else {
+      markActive($(this).data("id"));
+    }
+  });
+
+  // API POST REQUEST
+  liveDate();
+  getAndDisplayAllTasks();
 });
